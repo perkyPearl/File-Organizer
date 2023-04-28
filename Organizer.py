e203b86dab6b587cee14f4ab000ce3d6768b63f8
perkyPearl <pearl.dhiman2004@gmail.com>
2023-04-28 09:22:25 +0530

    Optimized the Code & Removed the Printing of Supported File Extensions

--- Organizer.py
+++ Organizer.py
@@ -3,7 +3,9 @@ import shutil
 FileCount = 0
 FolderCount = 0
 dic = {}
+left=[]
 
+#Reads the File.txt and updates the dic Dictionary
 try:
     file = open("FileExt.txt")
     for i in file.readlines():
@@ -12,24 +14,32 @@ try:
 except:
     pass
 
-print("Valid File Extensions: ")
-
-for i in dic.keys():
-    print(i,end=" | ")
+#Prints out the Supported File Extension
+#print("\nValid File Extensions: ",end='')
+#for i in dic.keys():
+#    print(i,end=" ")
+#print()
 
 files = os.listdir()
 path = os.getcwd()
 
 for i in files:
     if i != "Organizer.py" and i!="FileExt.txt":
-        file = i.split('.')
+        file = i.split('.')                                          #Splits the File Name and Extension
         if len(file) >= 2:
             fileExt = file[1]
-            if fileExt in dic.keys():
-                if not os.path.exists(path+"\\"+dic[fileExt]):
-                    os.mkdir(dic[fileExt])
+            if fileExt in dic.keys():                               #Checks the File Extension is supported 
+                if not os.path.exists(path+"\\"+dic[fileExt]):      #Checks the Folder exists
+                    os.mkdir(dic[fileExt])                          #Creates the Folder
                     FolderCount+=1
-            shutil.move(path+"\\"+i,path+"\\"+dic[fileExt])
-            FileCount+=1
+                shutil.move(path+"\\"+i,path+"\\"+dic[fileExt])     #Moves the File into the Folder
+                FileCount+=1
+            else:
+                left.append(i)                                      
 
-print(f"\n\nFiles Moved: {FileCount}\nFolder Created: {FolderCount}")
+if len(left) != 0:
+    print("Files Ignored: ")
+    for i in left:
+        print(i)
+
+print(f"\nFiles Moved: {FileCount}\nFolder Created: {FolderCount}")
