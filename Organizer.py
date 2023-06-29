e7031ed4362f279be90ae8f8d3b3d75697a69d45
Pearl <pearl.dhiman2004@gmail.com>
2023-06-29 20:17:50 +0530

    Updated and Optimized the Code

--- Organizer.py
+++ Organizer.py
@@ -1,45 +1,70 @@
 import os
 import shutil
+
 FileCount = 0
 FolderCount = 0
 dic = {}
+Folders = []
 left=[]
-
+logs = []
 #Reads the File.txt and updates the dic Dictionary
 try:
     file = open("FileExt.txt")
     for i in file.readlines():
         i = i.strip("\n").split(" : ")
+        Folders.append(i[0])
         dic.update({i[1]: i[0]})    
 except:
     pass
 
-#Prints out the Supported File Extension
-#print("\nValid File Extensions: ",end='')
-#for i in dic.keys():
-#    print(i,end=" ")
-#print()
+while True:
+    path = os.getcwd()
+    files = [i for i in os.listdir() if i not in Folders]
 
-files = os.listdir()
-path = os.getcwd()
+    files.remove("Organizer.py")
+    files.remove("FileExt.txt")
 
-for i in files:
-    if i != "Organizer.py" and i!="FileExt.txt":
-        file = i.split('.')                                          #Splits the File Name and Extension
-        if len(file) >= 2:
-            fileExt = file[1]
-            if fileExt in dic.keys():                               #Checks the File Extension is supported 
-                if not os.path.exists(path+"\\"+dic[fileExt]):      #Checks the Folder exists
-                    os.mkdir(dic[fileExt])                          #Creates the Folder
-                    FolderCount+=1
-                shutil.move(path+"\\"+i,path+"\\"+dic[fileExt])     #Moves the File into the Folder
-                FileCount+=1
-            else:
-                left.append(i)                                      
+    inp = input('\nType "help" to list out all commands\n>> ').lower()
 
-if len(left) != 0:
-    print("Files Ignored: ")
-    for i in left:
-        print(i)
+    if inp == 'start':
+        for i in files:
+            file = i.split('.')                                          #Splits the File Name and Extension
+            if len(file) >= 2:
+                fileExt = file[1]
+                if fileExt in dic.keys():                               #Checks the File Extension is supported 
+                    if not os.path.exists(path+"\\"+dic[fileExt]):      #Checks the Folder exists
+                        os.mkdir(dic[fileExt])                          #Creates the Folder
+                        FolderCount+=1
+                    shutil.move(path+"\\"+i,path+"\\"+dic[fileExt])     #Moves the File into the Folder
+                    FileCount+=1
+                    logs.append(f"{i} -> {dic[fileExt]}")
+                else:
+                    left.append(i)                                      
 
-print(f"\nFiles Moved: {FileCount}\nFolder Created: {FolderCount}")
+        if len(left) != 0:
+            print("\nFiles Ignored: ")
+            for i in left:
+                print(i)
+        print(f"\nFiles Moved: {FileCount}\nFolder Created: {FolderCount}")
+    
+    elif inp == 'status':
+        print("To be Sorted:")
+        for i in files:
+            print(i)
+
+    elif inp == "logs":
+        for i in logs:
+            print(i)
+    
+    elif inp == "help":
+        print('''
+start   : To Sort/Move the Files According to the File Extensions mentioned in the FileExt.txt.
+status  : To Printout the Files to that are going to be sorted.
+logs    : To Printout the Which File moved where. For that Particular Session/Instance.
+exit    : To Exit the Program.
+        ''')
+    elif inp == 'exit':
+        print("Exited")
+        break
+    else:
+        print("Invalid Command")
